a50857fa2870848cc3e4c6e6fd366a6f3211090f
Ben Larocque <benlaro0214@hotmail.com>
2021-03-10 22:39:49 -0500

    configured the server.js. Packages, server, and routing have been defined.

---
+++ server.js
@@ -0,0 +1,25 @@
+//Const & Packages
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require("body-parser"); 
+const exphbs = require("express-handlebars"); 
+
+
+//Express Server
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(express.static("public")); 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//Routing
+const routes = require("./controllers/burgersController.js");
+app.use("/", routes);
+
+//Server port and listening
+let PORT = process.env.PORT || 8080; // adjust this to 3001 for Heroku, 8080 for local dev.
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
