4a4e79d89b8c1126104564d963d3728ba296e0fa
Ben Larocque <benlaro0214@hotmail.com>
2021-03-11 22:23:50 -0500

    Uploading the schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date)
+VALUES
+(null, "Franken Burger", FALSE, null),
+(null, "Vlad the Burger", FALSE, null),
+(null, "Dragon Burger", FALSE, null),
+(null, "Double Dragon Burger", FALSE, null),
+(null, "Hellraiser Burger", FALSE, null),
+(null, "Kaiju Burger ", FALSE, null);
